95c2c2b9b032d8b2912cae8f8dbb2ba964285cb6
Deki Peltshog <dekipeltshogcodes@gmail.com>
2024-04-16 20:52:01 -0600

    first

--- app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
+++ app/src/main/java/com/driuft/sharedlifecycle/MainActivity.kt
@@ -1,17 +1,49 @@
 package com.driuft.sharedlifecycle
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var _sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _sharedPreferences = this.getSharedPreferences(this.packageName, Context.MODE_PRIVATE)
+        _sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
+
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onCreate")
+            apply()
+        }
+    }
+
+    private val listener =
+        SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            Toast.makeText(
+                this,
+                sharedPreferences.getString(
+                    key,
+                    resources.getString(R.string.default_state)
+                ),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    private fun unregisterListener() {
+        _sharedPreferences.unregisterOnSharedPreferenceChangeListener(listener)
     }
 
     override fun onStart() {
         super.onStart()
+        with (_sharedPreferences.edit()) {
+            putString(getString(R.string.current_lifecycle_state), "onStart")
+            apply()
+        }
     }
 
     override fun onResume() {
@@ -32,5 +64,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        unregisterListener()
     }
 }
